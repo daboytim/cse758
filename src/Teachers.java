6a41b849c366fdbd6af0ea97dc18669a75b1e0d6
Chao Lin <soullinc@gmail.com>
2012-05-01 17:34:29 -0400

    updated teacher/db class

--- src/Teachers.java
+++ src/Teachers.java
@@ -3,21 +3,19 @@ import java.util.ArrayList;
 
 public class Teachers implements Comparable<Teachers> {
 
-	private int id;
 	private String name;
-	// tracks the classes a teacher can teach. ordering most preferred in position 0.
+	// tracks the classes a teacher can teach. K-8:1-9
+	// ordering most preferred in position 0. changes while scheduling.
 	private ArrayList<Integer> preference;
+	// order does not matter. does not change. 
+	private ArrayList<Integer> capable;
 	private int cls;
 	private int room;
 	
-	public Teachers(int id, String name, ArrayList<Integer> preference){
-		this.id = id;
+	public Teachers(String name, ArrayList<Integer> classes){
 		this.name = name;
-		this.preference = preference;
-	}
-	
-	public int getId() {
-		return this.id;
+		this.preference = classes;
+		this.capable = new ArrayList<Integer>(classes);
 	}
 	
 	public String getName(){
@@ -27,10 +25,9 @@ public class Teachers implements Comparable<Teachers> {
 	public int getCls(){
 		return this.cls;
 	}
-	// feels unnecessary along with getCls() when can just use firstPref()
-	public void setCls(){
-		this.cls = this.preference.get(0);
-		for(int i = 1; this.preference.size() > 1; this.preference.remove(i));
+
+	public void setCls(Integer cls){
+		this.cls = cls;
 	}
 	
 	public int firstPref(){
@@ -44,6 +41,10 @@ public class Teachers implements Comparable<Teachers> {
 	// number of classes available to teach
 	public int availability(){
 		return this.preference.size();
+	}
+	
+	public boolean canTeach(Integer cls){
+		return this.capable.contains(cls);
 	}
 	
 	public void setRoom(int num){
