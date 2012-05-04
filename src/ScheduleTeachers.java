23dfb186cf7ee43644af86b6f569ca59886d61a8
Chao Lin <soullinc@gmail.com>
2012-05-03 20:16:19 -0400

    separated preference for math/read/la, and changed method formats. need to put a Type enum in as parameter for most of Teacher's methods.

--- src/ScheduleTeachers.java
+++ src/ScheduleTeachers.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ScheduleTeachers {
-
 	public static void assign(TeacherDB tch)
 	{
+		Teachers.Type type;
 		ArrayList<Teachers> teachers = tch.getTeachers();
 		ArrayList<Teachers> assigned = new ArrayList<Teachers>();
 		ArrayList<Teachers> unlucky = new ArrayList<Teachers>();
 		ArrayList<Integer> classes = new ArrayList<Integer>();
+		List<Classes> clsList;
+		int clsNum;
 		
 		/* === Algorithm ===
 		 * find teacher with minimum classes able to teach
@@ -24,194 +27,81 @@ public class ScheduleTeachers {
 		 *     and one unlucky teacher who can teach same class that A was assigned to, and swap
 		 */
 		
-		//=== math ===
-		while(teachers.size() > 0)
-		{
-			int min = findMin(teachers);
-			Teachers t = teachers.get(min);
-			int pref = t.firstPref();
-			int clsID = getCls(pref, classes, 0);
-			while(clsID < 0 && t.availability() > 0)
+		for (int classType = 0; classType < 3; classType++) {
+			if (classType == 0)
 			{
-				t.changePref(pref);
-				pref = t.firstPref();
-				clsID = getCls(pref, classes, 0);
+				type = Teachers.Type.MATH;
+				clsList = ClassFactory.mathClsLst;
+				clsNum = ClassFactory.getTotalMath();
 			}
-			if(t.availability() > 0)
+			else if (classType == 1)
 			{
-				t.setMathCls(pref, clsID);
-				classes.add(clsID);
-				assigned.add(teachers.remove(min));
+				type = Teachers.Type.READ;
+				clsList = ClassFactory.readClsLst;
+				clsNum = ClassFactory.getTotalRead();
 			}
 			else
 			{
-				unlucky.add(teachers.remove(min));
+				type = Teachers.Type.LA;
+				clsList = ClassFactory.laClsLst;
+				clsNum = ClassFactory.getTotalLA();
 			}
-		}
-		while(ClassFactory.getTotalMath() > classes.size())
-		{
-			int clsSize = classes.size();
-			int una = unassigned(classes, 1);
-			int lvl = ClassFactory.mathClsLst.get(una).getLvl();
-			int ID = ClassFactory.mathClsLst.get(una).getClsID();
-			for(int i = 0; i < assigned.size(); i++)
-			{
-				if(assigned.get(i).canTeach(lvl))
-				{
-					int assignedLvl = assigned.get(i).getMathClsLvl();
-					for(int j = 0; j < unlucky.size(); j++)
-					{
-						if(unlucky.get(j).canTeach(assignedLvl))
-						{
-							unlucky.get(j).setMathCls(assignedLvl, assigned.get(i).getMathClsID());
-							assigned.get(i).setMathCls(lvl, ID);
-							assigned.add(unlucky.remove(j));
-							classes.add(ID);
-							i = assigned.size();
-							break;
+			while(assigned.size() > 0)
+				teachers.add(assigned.remove(0));
+			while(unlucky.size() > 0)
+				teachers.add(unlucky.remove(0));
+			
+			while (teachers.size() > 0) {
+				int min = findMin(teachers, type);
+				Teachers t = teachers.get(min);
+				int pref = t.firstPref(type);
+				int clsID = getCls(pref, classes, type);
+				while (clsID < 0 && t.availability(type) > 0) {
+					t.changePref(pref, type);
+					pref = t.firstPref(type);
+					clsID = getCls(pref, classes, type);
+				}
+				if (t.availability(type) > 0) {
+					t.setCls(pref, clsID, type);
+					classes.add(clsID);
+					assigned.add(teachers.remove(min));
+				} else {
+					unlucky.add(teachers.remove(min));
+				}
+			}
+			while (clsNum > classes.size()) {
+				int clsSize = classes.size();
+				int una = unassigned(classes, type);
+				int lvl = clsList.get(una).getLvl();
+				int ID = clsList.get(una).getClsID();
+				for (int i = 0; i < assigned.size(); i++) {
+					if (assigned.get(i).canTeach(lvl, type)) {
+						int assignedLvl = assigned.get(i).getClsLvl(type);
+						for (int j = 0; j < unlucky.size(); j++) {
+							if (unlucky.get(j).canTeach(assignedLvl, type)) {
+								unlucky.get(j).setCls(assignedLvl,
+										assigned.get(i).getClsID(type), type);
+								assigned.get(i).setCls(lvl, ID, type);
+								assigned.add(unlucky.remove(j));
+								classes.add(ID);
+								i = assigned.size();
+								break;
+							}
 						}
 					}
 				}
+				if (clsSize == classes.size())
+					break;
 			}
-			if(clsSize == classes.size())
-				break;
-		}
-		
-		//=== Reading ===
-		classes.clear();
-		// move all teachers from assigned+unlucky to teachers for assigning to reading classes
-		while(assigned.size() > 0)
-			teachers.add(assigned.remove(0));
-		while(unlucky.size() > 0)
-			teachers.add(unlucky.remove(0));
-		for(int i = 0; i < teachers.size(); i++)
-		{
-			teachers.get(i).prefReset();
-		}
-		
-		while(teachers.size() > 0)
-		{
-			int min = findMin(teachers);
-			Teachers t = teachers.get(min);
-			int pref = t.firstPref();
-			int clsID = getCls(pref, classes, 1);
-			while(clsID < 0 && t.availability() > 0)
-			{
-				t.changePref(pref);
-				pref = t.firstPref();
-				clsID = getCls(pref, classes, 1);
-			}
-			if(t.availability() > 0)
-			{
-				t.setReadCls(pref, clsID);
-				classes.add(clsID);
-				assigned.add(teachers.remove(min));
-			}
-			else
-			{
-				unlucky.add(teachers.remove(min));
-			}
-		}
-		while(ClassFactory.getTotalRead() > classes.size())
-		{
-			int clsSize = classes.size();
-			int una = unassigned(classes, 1);
-			int lvl = ClassFactory.readClsLst.get(una).getLvl();
-			int ID = ClassFactory.readClsLst.get(una).getClsID();
-			for(int i = 0; i < assigned.size(); i++)
-			{
-				if(assigned.get(i).canTeach(lvl))
-				{
-					int assignedLvl = assigned.get(i).getReadClsLvl();
-					for(int j = 0; j < unlucky.size(); j++)
-					{
-						if(unlucky.get(j).canTeach(assignedLvl))
-						{
-							unlucky.get(j).setReadCls(assignedLvl, assigned.get(i).getReadClsID());
-							assigned.get(i).setReadCls(lvl, ID);
-							assigned.add(unlucky.remove(j));
-							classes.add(ID);
-							i = assigned.size();
-							break;
-						}
-					}
+
+
+			for (int i = 0; i < assigned.size(); i++) {
+				if (assigned.get(i).getClsID(Teachers.Type.MATH) <= 0
+						|| assigned.get(i).getClsID(Teachers.Type.READ) <= 0
+						|| assigned.get(i).getClsID(Teachers.Type.LA) <= 0) {
+					unlucky.add(assigned.remove(i));
+					i--;
 				}
-			}
-			if(clsSize == classes.size())
-				break;
-		}
-		
-		//=== LA ===
-		classes.clear();
-		// move all teachers from assigned+unlucky to teachers for assigning to reading classes
-		while(assigned.size() > 0)
-			teachers.add(assigned.remove(0));
-		while(unlucky.size() > 0)
-			teachers.add(unlucky.remove(0));
-		for(int i = 0; i < teachers.size(); i++)
-		{
-			teachers.get(i).prefReset();
-		}
-		
-		while(teachers.size() > 0)
-		{
-			int min = findMin(teachers);
-			Teachers t = teachers.get(min);
-			int pref = t.firstPref();
-			int clsID = getCls(pref, classes, 2);
-			while(clsID < 0 && t.availability() > 0)
-			{
-				t.changePref(pref);
-				pref = t.firstPref();
-				clsID = getCls(pref, classes, 2);
-			}
-			if(t.availability() > 0)
-			{
-				t.setLACls(pref, clsID);
-				classes.add(clsID);
-				assigned.add(teachers.remove(min));
-			}
-			else
-			{
-				unlucky.add(teachers.remove(min));
-			}
-		}
-		while(ClassFactory.getTotalLA() > classes.size())
-		{
-			int clsSize = classes.size();
-			int una = unassigned(classes, 1);
-			int lvl = ClassFactory.laClsLst.get(una).getLvl();
-			int ID = ClassFactory.laClsLst.get(una).getClsID();
-			for(int i = 0; i < assigned.size(); i++)
-			{
-				if(assigned.get(i).canTeach(lvl))
-				{
-					int assignedLvl = assigned.get(i).getLAClsLvl();
-					for(int j = 0; j < unlucky.size(); j++)
-					{
-						if(unlucky.get(j).canTeach(assignedLvl))
-						{
-							unlucky.get(j).setLACls(assignedLvl, assigned.get(i).getLAClsID());
-							assigned.get(i).setLACls(lvl, ID);
-							assigned.add(unlucky.remove(j));
-							classes.add(ID);
-							i = assigned.size();
-							break;
-						}
-					}
-				}
-			}
-			if(clsSize == classes.size())
-				break;
-		}
-		
-		for(int i = 0; i < assigned.size(); i++)
-		{
-			if(assigned.get(i).getLAClsID() <= 0 || assigned.get(i).getReadClsID() <= 0
-					|| assigned.get(i).getMathClsID() <= 0)
-			{
-				unlucky.add(assigned.remove(i));
-				i--;
 			}
 		}
 		//=== print outs for testing ===
@@ -224,81 +114,73 @@ public class ScheduleTeachers {
 	}
 	
 	// get unassigned class
-	// type: 0 = math, 1 = reading, 2 = la
-	private static int unassigned(ArrayList<Integer> classes, int type)
+	private static int unassigned(ArrayList<Integer> classes, Teachers.Type type)
 	{
-		if(type == 0)
-		{
+		switch(type) {
+		case MATH:
 			for(int i = 0; i < ClassFactory.getTotalMath(); i++)
 			{
 				if(!classes.contains(ClassFactory.mathClsLst.get(i).getClsID()))
 					return i;
 			
 			}
-		}
-		else if(type == 1)
-		{
+		case READ:
 			for(int i = 0; i < ClassFactory.getTotalRead(); i++)
 			{
 				if(!classes.contains(ClassFactory.readClsLst.get(i).getClsID()))
 					return i;
 			}
-		}
-		else
-		{
+		case LA:
 			for(int i = 0; i < ClassFactory.getTotalLA(); i++)
 			{
 				if(!classes.contains(ClassFactory.laClsLst.get(i).getClsID()))
 					return i;
 			}
+		default:
+			return -1;
 		}
-		return -1;
 	}
 	
 	// check if the unassigned classes contains teacher's preferred class
 	// returns classID if such class exists, else return -1
-	// type: 0 = math, 1 = reading, 2 = la
-	private static int getCls(int cls, ArrayList<Integer> assigned, int type)
+	private static int getCls(int cls, ArrayList<Integer> assigned, Teachers.Type type)
 	{
 		if(cls < 0)
 			return -1; 
-		if(type == 0)
-		{
+		switch(type) {
+		case MATH:
 			for(int i = 0; i < ClassFactory.getTotalMath(); i++)
 			{
 				if(ClassFactory.mathClsLst.get(i).getLvl() == cls 
 						&& !assigned.contains(ClassFactory.mathClsLst.get(i).getClsID()))
 					return ClassFactory.mathClsLst.get(i).getClsID();
 			}
-		}
-		else if(type == 1)
-		{
+		case READ:
 			for(int i = 0; i < ClassFactory.getTotalRead(); i++)
 			{
 				if(ClassFactory.readClsLst.get(i).getLvl() == cls
 						&& !assigned.contains(ClassFactory.readClsLst.get(i).getClsID()))
 					return ClassFactory.readClsLst.get(i).getClsID();
 			}
-		}
-		else
-		{
+		case LA:
 			for(int i = 0; i < ClassFactory.getTotalLA(); i++)
 			{
 				if(ClassFactory.laClsLst.get(i).getLvl() == cls
 						&& !assigned.contains(ClassFactory.laClsLst.get(i).getClsID()))
 					return ClassFactory.laClsLst.get(i).getClsID();
 			}
+		default:
+			return -1;
 		}
-		return -1;
 	}
 	
 	// find the teacher with fewest number of class able to teach
-	private static int findMin(ArrayList<Teachers> tch)
+	private static int findMin(ArrayList<Teachers> tch, Teachers.Type t)
 	{
 		int min = 0;
 		for(int i = 1; i < tch.size(); i++)
 		{
-			if(tch.get(i).compareTo(tch.get(min)) < 0)
+			if(tch.get(i).compareTo(tch.get(min), t) < 0)
 				min = i;
 		}
 		return min;
--- src/Teachers.java
+++ src/Teachers.java
@@ -1,26 +1,36 @@
 import java.util.ArrayList;
 
+public class Teachers {
 
-public class Teachers implements Comparable<Teachers> {
-
+	public enum Type {MATH, READ, LA}
 	private String name;
 	// tracks the classes a teacher can teach. K-8:1-9
 	// preference: ordering most preferred in position 0. changes while scheduling.
-	private ArrayList<Integer> preference; //TODO: math/read/la are most likely 3 separate sets
+	private ArrayList<Integer> mathPreference;
+	private ArrayList<Integer> readPreference;
+	private ArrayList<Integer> laPreference;
 	// capable: order does not matter. does not change. 
-	private ArrayList<Integer> capable;
+	private ArrayList<Integer> capableM;
+	private ArrayList<Integer> capableR;
+	private ArrayList<Integer> capableL;
 	private int clsIDM;
 	private int clsIDR;
 	private int clsIDL;
 	private int clsLvlM;
 	private int clsLvlR;
 	private int clsLvlL;
-	private int room;
+	private int roomM;
+	private int roomL;
+	private int roomR;
 	
-	public Teachers(String name, ArrayList<Integer> classes){
+	public Teachers(String name, ArrayList<Integer> math, ArrayList<Integer> read, ArrayList<Integer> la){
 		this.name = name;
-		this.preference = classes;
-		this.capable = new ArrayList<Integer>(classes);
+		this.mathPreference = math;
+		this.capableM = new ArrayList<Integer>(math);
+		this.readPreference = read;
+		this.capableR = new ArrayList<Integer>(read);
+		this.laPreference = la;
+		this.capableL = new ArrayList<Integer>(la);
 	}
 	
 	public Teachers(String name) {
@@ -35,97 +45,176 @@ public class Teachers implements Comparable<Teachers> {
 		name = n;
 	}
 	
-	public int getMathClsID(){
-		return this.clsIDM;
-	}
-	
-	public int getReadClsID(){
-		return this.clsIDR;
-	}
-	
-	public int getLAClsID(){
-		return this.clsIDL;
-	}
+	public int getClsID(Type t){
+		switch(t) {
+		case MATH:
+			return this.clsIDM;
 
-	public int getMathClsLvl(){
-		return this.clsLvlM;
-	}
-	
-	public int getReadClsLvl(){
-		return this.clsLvlR;
-	}
-	
-	public int getLAClsLvl(){
-		return this.clsLvlL;
-	}
-
-	public void setReadCls(Integer clsLvl, Integer clsID){
-		this.clsLvlR = clsLvl;
-		this.clsIDR = clsID;
-	}
-
-	public void setLACls(Integer clsLvl, Integer clsID){
-		this.clsLvlL = clsLvl;
-		this.clsIDL = clsID;
-	}
-	
-	public void setMathCls(Integer clsLvl, Integer clsID){
-		this.clsLvlM = clsLvl;
-		this.clsIDM = clsID;
-	}
-	
-	public int firstPref(){
-		if(this.preference.size() <= 0)
+		case READ:
+			return this.clsIDR;
+			
+		case LA:
+			return this.clsIDL;	
+			
+		default:
 			return -1;
-		return this.preference.get(0);
+		}
+	}
+
+	public int getClsLvl(Type t){
+		switch(t) {
+		case MATH:
+			return this.clsLvlM;
+
+		case READ:
+			return this.clsLvlR;
+			
+		case LA:
+			return this.clsLvlR;
+			
+		default:
+			return -1;			
+		}	
+	}
+
+	public void setCls(Integer clsLvl, Integer clsID, Type t){
+		switch(t) {
+		case MATH:
+			this.clsLvlM = clsLvl;
+			this.clsIDM = clsID;
+			
+		case READ:
+			this.clsLvlR = clsLvl;
+			this.clsIDR = clsID;
+			
+		case LA:
+			this.clsLvlL = clsLvl;
+			this.clsIDL = clsID;
+		}
 	}
 	
-	public void changePref(Integer cls){
-		this.preference.remove(cls);
+	public int firstPref(Type t){
+		switch(t) {
+		case MATH:
+			if(this.mathPreference.size() <= 0)
+				return -1;
+			return this.mathPreference.get(0);
+			
+		case READ:
+			if(this.readPreference.size() <=0)
+				return -1;
+			return this.readPreference.get(0);
+			
+		case LA:
+			if(this.laPreference.size() <=0)
+				return -1;
+			return this.laPreference.get(0);
+			
+		default:
+			return -1;
+		}
+	}
+	
+	// changes/removes preference;
+	public void changePref(Integer cls, Type t){
+		switch(t) {
+		case MATH:
+			this.mathPreference.remove(cls);
+		case READ:
+			this.readPreference.remove(cls);
+		case LA:
+			this.laPreference.remove(cls);
+		}
 	}
 	
 	// number of classes available to teach
-	public int availability(){
-		return this.preference.size();
+	public int availability(Type t){
+		switch(t) {
+		case MATH:
+			return this.mathPreference.size();
+		case READ:
+			return this.readPreference.size();
+		case LA:
+			return this.laPreference.size();
+		default:
+			return -1;
+		}
 	}
 	
-	public boolean canTeach(Integer cls){
-		return this.capable.contains(cls);
+	public boolean canTeach(Integer cls, Type t){
+		switch(t) {
+		case MATH:
+			return this.capableM.contains(cls);
+		case READ:
+			return this.capableR.contains(cls);
+		case LA:
+			return this.capableL.contains(cls);
+		default:
+			return false;
+		}
 	}
 	
-	public void setRoom(int num){
-		this.room = num;
+	public void setRoom(int num, Type t){
+		switch(t) {
+		case MATH:
+			this.roomM = num;
+		case READ:
+			this.roomR = num;
+		case LA:
+			this.roomL = num;
+		}
 	}
 	
-	public int getRoom(){
-		return room;
+	public int getRoom(Type t){
+		switch(t) {
+		case MATH:
+			return this.roomM;
+		case READ:
+			return this.roomR;
+		case LA:
+			return this.roomL;
+		default:
+			return -1;
+		}
 	}
 	
-	// take this out if math/reading/la have different preferences
-	public void prefReset() {
-		preference = new ArrayList<Integer>(capable);
+	public ArrayList<Integer> getPreference(Type t) {
+		switch(t) {
+		case MATH:
+			return this.mathPreference;
+		case READ:
+			return this.readPreference;
+		case LA:
+			return this.laPreference;
+		default:
+			return null;
+		}
 	}
 	
-	public ArrayList<Integer> getPreference() {
-		return preference;
-	}
-	
-	public void setPreference(ArrayList<Integer> p) {
-		preference = p;
-		capable = new ArrayList<Integer>(p);
+	public void setPreference(ArrayList<Integer> p, Type t) {
+		switch(t) {
+		case MATH:
+			this.mathPreference = p;
+			this.capableM = new ArrayList<Integer>(p);
+		case READ:
+			this.readPreference = p;
+			this.capableR = new ArrayList<Integer>(p);
+		case LA:
+			this.laPreference = p;
+			this.capableR = new ArrayList<Integer>(p);
+		}
 	}
 	
 	// returns n > 0, if this.teacher has more available classes compare to teacher
-	@Override
-	public int compareTo(Teachers teacher) {
-		return this.availability()-teacher.availability();
+	public int compareTo(Teachers teacher, Type t) {
+		return this.availability(t)-teacher.availability(t);
 	}
 	
 	@Override
 	public String toString() {
-		return name + " :: Room: " +room+ "\n\tMath: lv " + clsLvlM + ", ClassID: " + clsIDM +
-		"\n\tRead: lv " + clsLvlR + ", ClassID: " + clsIDR +
-		"\n\tLA  : lv " + clsLvlL + ", ClsssID: " + clsIDL;
+		return name + "\n\tMath: lv " + clsLvlM + ", ClassID: " + clsIDM + ", Room: " + roomM +
+		"\n\tRead: lv " + clsLvlR + ", ClassID: " + clsIDR + ", Room: " + roomR +
+		"\n\tLA  : lv " + clsLvlL + ", ClsssID: " + clsIDL + ", Room: " + roomL;
 	}
 
 }
