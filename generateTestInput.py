f0712b8b8d96405a105d8a3fdb5d79eb233c0770
Kai Li <iverstyle@gmail.com>
2012-05-07 17:29:05 -0400

    dates and month with single digit now have a leading 0

--- generateTestInput.py
+++ generateTestInput.py
@@ -2,5 +2,12 @@ import random
 
 f= open('testInput','w')
 for i in range(0,100):
-	f.write(str(i)+",Student,No."+str(i)+","+str(random.randint(1990,2012))+"-"+str(random.randint(1,12))+"-"+str(random.randint(1,28))+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(1,3))+"\n")
+	year = random.randint(1990,2012)
+	month = random.randint(1,12)
+	if month<10:
+		month='0'+str(month)
+	date = random.randint(1,28)
+	if date<10:
+		date = '0' +str(date)
+	f.write(str(i)+",Student,No."+str(i)+","+str(year)+"-"+str(month)+"-"+str(date)+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(1,3))+"\n")
 f.close()
