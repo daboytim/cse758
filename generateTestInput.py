56a680e3b4facdbd56aad4d2d18934359b9d0ff5
Kai Li <iverstyle@gmail.com>
2012-05-08 12:36:40 -0400

    python program updated

--- generateTestInput.py
+++ generateTestInput.py
@@ -1,8 +1,18 @@
 import random
+import sys
 
-f= open('testInput','w')
-for i in range(0,100):
-	year = random.randint(1990,2012)
+if (len(sys.argv)>1):
+	fileName = sys.argv[1]
+else:
+	fileName = raw_input("Input output file name:")
+
+numOfRecord = input("Input number of test cases:")
+minYear = input("Input lower limit of age(YYYY):")
+maxYear = input("Input upper limit of age(YYYY):")
+
+f= open(fileName,'w')
+for i in range(0,int(numOfRecord)):
+	year = random.randint(minYear,maxYear)
 	month = random.randint(1,12)
 	if month<10:
 		month='0'+str(month)
@@ -11,3 +21,4 @@ for i in range(0,100):
 		date = '0' +str(date)
 	f.write(str(i)+",Student,No."+str(i)+","+str(year)+"-"+str(month)+"-"+str(date)+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(0,8))+","+str(random.randint(1,3))+"\n")
 f.close()
+print "Record written to file:"+fileName
